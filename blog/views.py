327866ed66662e10894114a939d4d7beb4be92ff
whitesandss <whitesands@snu.ac.kr>
2018-11-06 03:25:39 +0900

    Midfeid

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.utils import timezones
+
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
